95d32cb87703c1c36d2c3e04d9f96667ccbbca54
shruti29s <shruti29s06@gmail.com>
2020-10-18 16:19:25 +0530

    Added opportunities in technology domain

--- tech.php
+++ tech.php
@@ -26,5 +26,42 @@
     style="text-align:center; text-shadow: 2px 2px 5px rgb(255, 0, 128); font-family:Stencil Std, fantasy; color:whitesmoke;">
     Jobs Opportunities
   </h1>
+  <div class="row">
+  <div class="column">
+    <img src="https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/978647f6cea2a25bf85a9ebcefe5d3a9" alt="Ericsson" style="margin:auto;margin-top: 20px;margin-left: 100px;padding: 10px;width: 200px">
+    <h5 align="center" style="color:#FF63B1;margin-left:100px;padding: 10px">Assisstant Engineer</h5>
+    <a href="https://www.indeed.co.in/rc/clk?jk=e1824ddde9bb79be&from=vj&pos=bottom&astse=b1e6dd00f54b5e08&assa=8508"><h6 align="center" style="color:#FF63B1;margin-left:100px;padding: 10px">Apply</h6></a>
+  </div>
+  <div class="column">
+    <img src="https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/1f1b2d38ff6fb55739d8c2010880edbe" alt="epiq" style="margin: auto;margin-top: 20px;margin-left: 200px;padding: 10px;width: 200px">
+    <h5 align="center" style="color:#FF63B1;margin-left:180px;padding: 10px">Customer Support Specialist</h5>
+    <a href="https://www.indeed.co.in/rc/clk?jk=ade191dc77941be9&from=vj&pos=bottom&astse=e3fea5c0cab11800&assa=9818"><h6 align="center" style="color:#FF63B1;margin-left:200px;padding: 10px">Apply</h6></a>
+  </div>
+  <div class="column">
+    <img src="https://www.alcindia.org/assets/logo-image-1-9d62fd16134e6c92044422c9c97be401.jpg" alt="ALC India" style="margin: auto;margin-top: 20px;margin-left: 200px;padding: 10px;height:200px;width: 200px">
+    <h5 align="center" style="color:#FF63B1;margin-left:180px;padding: 10px">Project Executive</h5>
+    <a href="https://www.indeed.co.in/rc/clk?jk=2c9d76967a9d8df0&from=vj&pos=bottom&astse=436f0aa93424b2f7&assa=2848"><h6 align="center" style="color:#FF63B1;margin-left:200px;padding: 10px">Apply</h6></a>
+  </div>
+</div>
+<hr>
+<h1 style="text-align:center; text-shadow: 2px 2px 5px rgb(255, 0, 128); font-family:Stencil Std, fantasy; color:whitesmoke;">
+    Study Opportunities</h1>
+    <div class="row">
+  <div class="column">
+    <img src="https://1.bp.blogspot.com/-qH4IfGvl8_4/XylJdJy-XdI/AAAAAAAAGAY/-ojaM0emuNMg9BwByjajPecOZSMXwhfYQCLcBGAsYHQ/s1600/images.jpg" alt="Tagore International School" style="margin:auto;margin-top: 20px;margin-left: 100px;padding: 10px;width: 200px">
+    <h5 align="center" style="color:#FF63B1;margin-left:75px;padding: 10px">Tagore International School</h5>
+    <a href="https://tagoreint.com/"><h6 align="center" style="color:#FF63B1;margin-left:100px;padding: 10px">Apply</h6></a>
+  </div>
+  <div class="column">
+    <img src="https://media.9curry.com/uploads/organization/image/94/jnu-logo.png" alt="JNU" style="margin: auto;margin-top: 20px;margin-left: 200px;padding: 10px;width: 200px">
+    <h5 align="center" style="color:#FF63B1;margin-left:180px;padding: 10px">Jawarharlal Nehru University</h5>
+    <a href="https://www.jnu.ac.in/main/"><h6 align="center" style="color:#FF63B1;margin-left:200px;padding: 10px">Apply</h6></a>
+  </div>
+  <div class="column">
+    <img src="https://www.noticebard.com/wp-content/uploads/2019/08/IITKGP-4.png" alt="IIT Kharagpur" style="margin: auto;margin-top: 20px;margin-left: 200px;padding: 10px;height:200px;width: 200px">
+    <h5 align="center" style="color:#FF63B1;margin-left:180px;padding: 10px">IIT Kharagpur</h5>
+    <a href="http://www.iitkgp.ac.in/"><h6 align="center" style="color:#FF63B1;margin-left:200px;padding: 10px">Apply</h6></a>
+  </div>
+</div>
 </body>
 </html>
