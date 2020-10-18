0d9b44686b3e2b2f81dc520c10dd1d39d6953561
shruti29s <shruti29s06@gmail.com>
2020-10-18 18:04:09 +0530

    Added opportunities in sports

--- sports.php
+++ sports.php
@@ -26,5 +26,27 @@
     style="text-align:center; text-shadow: 2px 2px 5px rgb(255, 0, 128); font-family:Stencil Std, fantasy; color:whitesmoke;">
     Jobs Opportunities
   </h1>
+  <div class="row">
+  <div class="column">
+    <img src="https://media.glassdoor.com/sqll/2863945/elevate-sports-ventures-squarelogo-1582547336345.png" alt="Elevate Sports Venture" style="margin:auto;margin-top: 20px;margin-left: 300px;padding: 10px;width: 200px">
+    <h5 align="center" style="color:#FF63B1;margin-left:300px;padding: 10px">Account Executive</h5>
+    <a href="https://www.indeed.com/rc/clk?jk=ece8c06458018c88&from=vj&pos=bottom&jobsearchTk=1ektq9l6ro2j9800&sjdu=YmZE5d5THV8u75cuc0H6Y26AwfY51UOGmh3Z9h4OvXiqEwjVmF3LqQuVbVdKa84fl8EZNoFLzyHkpvcd7-Y9bg&astse=6ee007c04a531de2&assa=2058"><h6 align="center" style="color:#FF63B1;margin-left:300px;padding: 10px">Apply</h6></a>
+  </div>
+  <div class="column">
+    <img src="https://teamwork-online-production.s3.amazonaws.com/uploads/thumb_576d6dd8-24d5-4a21-9186-a20b89f70773.jpg" alt="St. Louis City" style="margin: auto;margin-top: 20px;margin-left: 300px;padding: 10px;height:200px;width: 200px">
+    <h5 align="center" style="color:#FF63B1;margin-left:300px;padding: 10px">Academy Director</h5>
+    <a href="https://www.indeed.com/rc/clk?jk=3d45f05b55341f69&from=vj&pos=bottom&jobsearchTk=1ektmcdaloc8k800&sjdu=YmZE5d5THV8u75cuc0H6Y26AwfY51UOGmh3Z9h4OvXibemNVRlBZq48Ey3ezNo9RnUUKYs5yKqp3Fg7KgmoxhA&astse=4d8d3f0020c21e15&assa=5440"><h6 align="center" style="color:#FF63B1;margin-left:300px;padding: 10px">Apply</h6></a>
+  </div>
+</div>
+<hr>
+<h1 style="text-align:center; text-shadow: 2px 2px 5px rgb(255, 0, 128); font-family:Stencil Std, fantasy; color:whitesmoke;">
+    Study Opportunities</h1>
+    <div class="row">
+  <div class="column">
+    <img src="https://27x1l838yl2blbjp91dovc81-wpengine.netdna-ssl.com/wp-content/uploads/2016/06/lgbt-sportsafe-logo.png" alt="LGBT SportsSafe" style="margin: auto;margin-top: 20px;margin-left: 600px;padding: 10px;width: 200px">
+    <h5 align="center" style="color:#FF63B1;margin-left:600px;padding: 10px">LGTB SportsSafe</h5>
+    <a href="https://lgbtsportsafe.com/"><h6 align="center" style="color:#FF63B1;margin-left:600px;padding: 10px">Apply</h6></a>
+  </div>
+</div>
 </body>
 </html>
